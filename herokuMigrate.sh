01e16c02a36d73e8e5d8be5005b3672a5cc079cf
Nicolas Fredrickson <fredricksonnnicolas@gmail.com>
2015-12-12 23:51:40 -0800

    Finalizing scripts.

--- herokuMigrate.sh
+++ herokuMigrate.sh
@@ -1,4 +1,4 @@
 heroku pg:reset --app serene-reaches-9115 DATABASE --confirm serene-reaches-9115
-heroku run --app serene-reaches-9115 rake db:setup
+heroku run --app serene-reaches-9115 rake db:setup RAILS_ENV=production
 heroku run --app serene-reaches-9115 rake db:migrate RAILS_ENV=production
 heroku restart --app serene-reaches-9115
---
+++ localMigrate.sh
@@ -0,0 +1,2 @@
+rake db:create RAILS_ENV=production
+rake db:migrate RAILS_ENV=production
